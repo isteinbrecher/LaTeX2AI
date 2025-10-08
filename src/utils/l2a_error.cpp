d5f7684eabee9cf4610397aafa52a97472b5cc15
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2025-10-08 11:10:22 +0200

    Improve error message for SetPlacedItemPath

--- src/utils/l2a_error.cpp
+++ src/utils/l2a_error.cpp
@@ -149,9 +149,9 @@ ai::UnicodeString L2A::ERR::AIErrorCodeToString(const int& err)
  */
 ai::UnicodeString L2A::ERR::AIErrorCodeToErrorString(const int& err)
 {
-    ai::UnicodeString error_string("Ilustrator Error.\nError code (int): ");
+    ai::UnicodeString error_string("Illustrator Error.\nError code (int): ");
     error_string += L2A::UTIL::IntegerToString(err);
-    error_string += "\nError code: ";
+    error_string += "\nError code (AI): ";
     error_string += AIErrorCodeToString(err);
     return error_string;
 }
